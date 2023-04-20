fed19f3a6d6e832c5f32c2d5ba2dd0f3f8f75877
Adrián <supadri2002@gmail.com>
2023-04-20 09:47:46 +0200

    He borrado el contenido de la API ya que Ricard se estñá encargando de hacerla

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-const fetchData = async () => {
-    const config = {
-        headers: {
-            Accept: 'application/json',
-        },
-    }
-    const response = await fetch("", config);
-    const data = await response.json();
-    pintarApi(data)
-    console.log(data)
-}
+
 
 
