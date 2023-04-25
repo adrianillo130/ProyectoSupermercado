e1cc9b1534bdbf840b4a8a79fb08b84bd69a75d4
Adrián <supadri2002@gmail.com>
2023-04-25 14:03:42 +0200

    Estoy empezando a añadir cosas al javascript del html de vistaprincipal y el html he comentado todo

---
+++ VistaPrincipal.js
@@ -0,0 +1,35 @@
+const FeaturedProducts = document.getElementById("FeaturedProducts")
+const sectionFPName = document.createElement("h2")
+sectionFPName.innerHTML = "Rebajas temporales"
+const containercard = document.createElement("div")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    console.log(FeaturedProducts)
