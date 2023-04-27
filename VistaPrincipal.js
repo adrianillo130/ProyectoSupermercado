a24fb77abb89bbcabdb475f49a301d92940784ec
Adrián <supadri2002@gmail.com>
2023-04-27 12:30:20 +0200

    He descomentado el html de VistPrincipal

--- VistaPrincipal.js
+++ VistaPrincipal.js
@@ -1,37 +1,59 @@
-const FeaturedProducts = document.getElementById("FeaturedProducts")
+const TemporarySales = document.getElementById("TemporarySales")
 const sectionFPName = document.createElement("h2")
-document.getElementsByClassName("sectionFP-name")
+sectionFPName.classList.add("sectionFP-name")
 sectionFPName.innerHTML = "Rebajas temporales"
-const containercard = document.createElement("div")
+const containerCard = document.createElement("div")
+containerCard.classList.add("containerCard")
+TemporarySales.append(sectionFPName, containerCard)
+
+//A partir de aquí se repite
+
+clase = [
+    {
+        Nombre: 'Adrian',
+        Edad: 20,
+        Nacionalidad: 'Española',
+    },
+    {
+        Nombre: 'Ricard',
+        Edad: 25,
+        Nacionalidad: 'Española',
+    },
+    {
+        Nombre: 'Carlos',
+        Edad: 20,
+        Nacionalidad: 'Española',
+    },
+    {
+        Nombre: 'Paola',
+        Edad: 25,
+        Nacionalidad: 'Mundo',
+    }
+]
+
+clase.forEach(element => {
+    const cardProduct = document.createElement("div")
+    cardProduct.classList.add("cardProduct")
+    containerCard.append(cardProduct)
+    cardProduct.innerHTML = `<img src="" alt="" class="productImage">
+                            <h3 class="productNaming">${element.Nombre}</h3>
+                            <p class="productSpecification">${element.Edad}</p>
+                            <p class="productPrice">${element.Nacionalidad}</p>
+                            <button class="plusBasketButton"></button>`
+});
 
 
 
 
+//Productos De Temporada
+
+const SeasonProducts = document.getElementById("SeasonProducts")
+const SectionFPName = document.createElement("h2")
+SectionFPName.classList.add("sectionFP-name")
+SectionFPName.innerHTML = "Productos De Temporada"
+SeasonProducts.appendChild(SectionFPName)
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    console.log(FeaturedProducts)
+console.log(TemporarySales)
+console.log(SeasonProducts)
