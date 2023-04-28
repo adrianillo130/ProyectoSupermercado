93bb7d83538cf4c59f018f9c360403729f7aaa86
paoguro <paolaguro25@gmail.com>
2023-04-28 12:28:20 +0200

    imagenes pintadas

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const giveMeAccessKey = async () => {
         },
         body: "grant_type=client_credentials&scope=product.compact",
     }).then((response) => response.json());
-    //console.log(res)
+    console.log(res)
     return res.access_token;
   };
   
@@ -26,7 +26,7 @@ const giveMeAccessKey = async () => {
     }
     })*/
 
-    let input = 'milk';
+    let input = 'meat';
     const productsUrl = `https://api.kroger.com/v1/products?filter.term=${input}&filter.locationId=01400943`; // cambiar el term "milk" por el producto deseado (variable?)
     const accessToken = await giveMeAccessKey();
     const productsResponse = fetch(productsUrl, {
@@ -39,6 +39,7 @@ const giveMeAccessKey = async () => {
     })
       .then((response) => response.json())
       .then((dataApi) => {
+        console.log(dataApi)
         let productosArray = dataApi.data;
         pintarCard(productosArray);
       })
@@ -54,24 +55,23 @@ const giveMeAccessKey = async () => {
 const pintarCard = productosArray => {
   const setionNews = document.getElementById('sectionNews')
   const h2News = document.createElement('h2')
-  h2News.innerHTML = 'Novedades'
+  h2News.innerHTML = 'Productos'
   sectionNews.appendChild(h2News)
   
   console.log(productosArray)
   
   productosArray.forEach(product => {
-    
     const cardProduct = document.createElement('div');
     cardProduct.classList.add('cardProduct')
     cardProduct.innerHTML = `
-                    <img src="${product.images}" class="productImage">
+                    <img src="${product.images[0].sizes[1].url}" class="productImage">
                     <h3 class="productNaming">${product.brand}</h3>
                     <p class="productSpecification">${product.description}</p>
                     <p class="productPrice">${product.items[0].price.regular}</p>
                     <button class="plusBasketButton" id="plusBasketButton">Añadir a la Cesta</button>` // este botón debería de ejecutar la función añadir a la cesta
     sectionNews.appendChild(cardProduct)
   });
-  //console.log(setionNews)
+  console.log(setionNews)
 }
 
 
